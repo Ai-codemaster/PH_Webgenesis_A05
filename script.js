78704e873514b02d756379a2b9da7e84e219a384
AI-Codemaster <ai5445359@gmail.com>
2025-11-02 22:33:41 +0600

    Assignment 05

---
+++ script.js
@@ -0,0 +1,16 @@
+// const langBg = document.getElementById('lang-bg');
+// const btnEn = document.getElementById('btn-en');
+// const btnBn = document.getElementById('btn-bn');
+
+// // default active button (English)
+// let activeBtn = btnEn;
+// langBg.style.left = btnEn.offsetLeft + 'px';
+
+// function moveBg(targetBtn) {
+//   langBg.style.left = targetBtn.offsetLeft + 'px';
+//   activeBtn = targetBtn;
+// }
+
+// // event listeners
+// btnEn.addEventListener('click', () => moveBg(btnEn));
+// btnBn.addEventListener('click', () => moveBg(btnBn));
